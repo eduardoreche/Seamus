33232f3b8f969f30911d5162b920dc025f362f56
Eduardo Reche <edu.reche@gmail.com>
2012-02-22 21:06:44 -0200

    Setting up a new rails app. Copy config/database.yml.sample to config/database.yml and customize.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seamus::Application.config.secret_token = '8caa145e78804efc466fb781d4b0f9a36ee4d784af04db2cd616f67a051c558fede09c65794f0fb1a448909797bb0b04bc40326ec50e7530315c77882549060d'
